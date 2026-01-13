3c26bbf207ddf5ea88940b115498e805d7f10a4d
BaariAbdul03 <abdulbaarihindustani@gmail.com>
2026-01-13 17:18:40 +0530

    Updated build.sh to initialize tables and created init_db.py

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ set -o errexit
 echo "Installing Python Dependencies..."
 pip install -r requirements.txt
 
+echo "Initializing Database..."
+python init_db.py
+
 echo "Building React Frontend..."
 cd rhymic-react
 npm install
