8fed082999b7e11463fefcae561281a855140208
BaariAbdul03 <abdulbaarihindustani@gmail.com>
2026-01-13 17:32:22 +0530

    Fixed database to scan for songs

--- init_db.py
+++ init_db.py
@@ -1,6 +1,10 @@
-from app import app, db
+from app import app, db, scan_library
 
 print("Initializing Database...")
 with app.app_context():
     db.create_all()
     print("Database tables created successfully!")
+    
+    print("Scanning Library...")
+    scan_library()
+    print("Library scan complete!")
